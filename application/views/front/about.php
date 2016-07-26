a74a131b6c8280ba0a9a78b14e473f2df802e1f5
littlejietu <littlejietu@qq.com>
2016-07-26 20:19:33 +0800

    service

--- application/views/front/about.php
+++ application/views/front/about.php
@@ -32,8 +32,8 @@
         <div class="container">
           <div class="row">            
             <ol class="breadcrumb hidden-xs">
-              <li><a href="/">首页</a></li>
-              <li class="active"><?php echo $nav['page_title'];?></li>
+              <li><a href="<?php echo base_url();?>">首页</a></li>
+              <li class="active"><?php echo $nav['page_title']?></li>
             </ol>                      
           </div> <!-- / .row -->
         </div> <!-- / .container -->
@@ -43,16 +43,11 @@
 
         <div class="row">
           <div class="col-sm-7">
-            <h3 class="first-child"><span>关于米荣公司</span></h3>
+            <h3 class="first-child"><span>杭州米荣网络科技有限公司</span></h3>
             <p>
-              米荣科技公司 
-              <br /><br />
-              米荣科技公司
-              <br /><br />
-              米荣科技公司
-              <br /><br />
+               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;专注于App开发、微信开发等移动互联网相关领域，通过严格管理规范和专业化服务为客户提供优质的手机App研发服务。公司由多位移动互联网资深从业精英创立，多年的技术沉淀，丰富的行业经验打造了一支技术实力雄厚的开发团队，为合作伙伴提供优质的定制服务。
             </p>
-            <a class="btn-animate btn-style btn-d btn-primary" href="contact-us.html">联系我们</a>
+            <a class="btn-animate btn-style btn-d btn-primary" href="<?php echo base_url()?>contact">联系我们</a>
             <br /><br />
           </div>
           <div class="col-sm-5">
@@ -85,75 +80,107 @@
             </div>
           </div>
         </div> <!-- / .row -->
-        <div class="row our-team-p">
-          <div class="col-sm-12">
-            <h3 class="headline"><span>我们的团队</span></h3>
-            <div class="row">
-              <div class="col-xs-12 col-sm-3 col-md-3">
-                <div class="team-member text-center">
-                  <img class="img-responsive center-block" src="<?php echo _get_cfg_path('img');?>team/team1.jpg" alt="...">
-                  <div class="info">
-                    <h3><strong>John Doe</strong></h3>
-                    <p class="text-muted">CEO / Founder</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed magna vel velit dignissim luctus eu in urna.</p>
-                    <ul class="list-inline text-muted">
-                      <li><a href="#"><i class="fa fa-facebook"></i></a></li>           
-                      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>               
-                      <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                      <li><a href="#"><i class="fa fa-skype"></i></a></li>              
-                    </ul>
-                  </div>                  
+        <div class="row">
+          <div class="maxapps-develop-server">
+            <div class="maxapps-develop-server-content">
+              <div class="develop-server-left">
+                <h1>我们的服务</h1>
+                <img class="star_big" src="<?php echo _get_cfg_path('img');?>maxapps_09-10.png" alt=""><img class="star_little" src="<?php echo _get_cfg_path('img');?>maxapps_27.png" alt="">
+                <p class="server-info">
+                  我们提供专业的 APP 开发服务
+                </p>
+                <p class="server-info">
+                  从前期的需求梳理到各类应用商城的软件上架
+                </p>
+                <p class="server-info">
+                  提供一站式移动 APP 开发解决方案
+                </p>
+                <div class="server-icons">
+                  <img src="<?php echo _get_cfg_path('img');?>maxapps-icons-1.png" alt=""><img src="<?php echo _get_cfg_path('img');?>maxapps-icons-2.png" alt=""><img src="<?php echo _get_cfg_path('img');?>maxapps-icons-3.png" alt=""><img src="<?php echo _get_cfg_path('img');?>maxapps-icons-4.png" alt=""><img src="<?php echo _get_cfg_path('img');?>maxapps-icons-5.png" alt="">
                 </div>
               </div>
-              <div class="col-xs-12 col-sm-3 col-md-3">
-                <div class="team-member text-center">
-                  <img class="img-responsive center-block" src="<?php echo _get_cfg_path('img');?>team/team2.jpg" alt="...">
-                  <div class="info">
-                    <h3><strong>Betty</strong></h3>
-                    <p class="text-muted">Developer</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed magna vel velit dignissim luctus eu in urna.</p>
-                    <ul class="list-inline">
-                      <li><a href="#"><i class="fa fa-facebook"></i></a></li>           
-                      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>               
-                      <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                      <li><a href="#"><i class="fa fa-skype"></i></a></li>              
-                    </ul>
-                  </div>
-                </div>
+              <div class="develop-server-center">
+                <ul>
+                  <li class="develop-server-center-h">流程</li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                  <li class="center-icons"><img src="<?php echo _get_cfg_path('img');?>maxapps_Process.png" alt=""></li>
+                </ul>
               </div>
-              <div class="col-xs-12 col-sm-3 col-md-3">
-                <div class="team-member text-center">
-                  <img class="img-responsive center-block" src="<?php echo _get_cfg_path('img');?>team/team3.jpg" alt="...">
-                  <div class="info">
-                    <h3><strong>Martin</strong></h3>
-                    <p class="text-muted">Photographer</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed magna vel velit dignissim luctus eu in urna.</p>
-                    <ul class="list-inline">
-                      <li><a href="#"><i class="fa fa-facebook"></i></a></li>           
-                      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>               
-                      <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                      <li><a href="#"><i class="fa fa-skype"></i></a></li>              
-                    </ul>
+              <div class="develop-server-right">
+                <ul class="develop-server-right-icons">
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-1.png" alt="">
+                  <div class="Process-info">
+                    <h4><b>确认需求</b></h4>
+                    <p>
+                      有专案经理与您沟通整理 APP 功能需求
+                    </p>
                   </div>
-                </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-2.png" alt="">
+                  <div class="Process-info">
+                    <h4>报价签约</h4>
+                    <p>
+                      签署保密 NDA ,保证您的信息安全
+                    </p>
+                  </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-3.png" alt="">
+                  <div class="Process-info">
+                    <h4>UI 设计</h4>
+                    <p>
+                      专业团队参与设计,修改到您满意为止
+                    </p>
+                  </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-4.png" alt="">
+                  <div class="Process-info">
+                    <h4>软件开发</h4>
+                    <p>
+                      依据需求复杂程度开发 APP
+                    </p>
+                  </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-5.png" alt="">
+                  <div class="Process-info">
+                    <h4>期中报告</h4>
+                    <p>
+                      项目进度汇报,让您实时掌握进度
+                    </p>
+                  </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-6.png" alt="">
+                  <div class="Process-info">
+                    <h4>测试修正</h4>
+                    <p>
+                      正面、压力测试,保证 APP 正常使用
+                    </p>
+                  </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-7.png" alt="">
+                  <div class="Process-info">
+                    <h4>结案阶段</h4>
+                    <p>
+                      交付客户产品及源码
+                    </p>
+                  </div>
+                  </li>
+                  <li><img src="<?php echo _get_cfg_path('img');?>maxapps_Process-8.png" alt="">
+                  <div class="Process-info">
+                    <h4>APP 上线</h4>
+                    <p>
+                      保障您的APP在苹果、安卓等市场上线
+                    </p>
+                  </div>
+                  </li>
+                </ul>
               </div>
-              <div class="col-xs-12 col-sm-3 col-md-3">
-                <div class="team-member text-center">
-                  <img class="img-responsive center-block" src="<?php echo _get_cfg_path('img');?>team/team4.jpg" alt="...">
-                  <div class="info">
-                    <h3><strong>Alex</strong></h3>
-                    <p class="text-muted">Web Designer</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed magna vel velit dignissim luctus eu in urna.</p>
-                    <ul class="list-inline">
-                      <li><a href="#"><i class="fa fa-facebook"></i></a></li>           
-                      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>               
-                      <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                      <li><a href="#"><i class="fa fa-skype"></i></a></li>              
-                    </ul>
-                  </div>
-                </div>
-              </div>              
-            </div> <!-- / .row -->
+            </div>
           </div>
         </div> <!-- / .row -->
 
@@ -161,30 +188,9 @@
 
     </div> <!-- / .wrapper -->
 
-   <?php include("inc/footer.php");?>
+  <?php include("inc/footer.php");?>
     
-    <!-- Copyright -->
-    <div class="copyrights">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="copyright">
-                <small>Copyright &copy; 2015.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></small>
-              </div>
-            </div>
-          </div> <!-- / .row -->
-        </div> <!-- / .container --> 
-    </div><!-- / .Copyright -->
   <?php echo _get_html_cssjs('js','jquery-1.11.0.min.js,bootstrap.min.js,scrolltopcontrol.js,SmoothScroll.js,custom.js','js');?>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <!-- <script src="js/jquery-1.11.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scrolltopcontrol.js"></script>
-    <script src="js/SmoothScroll.js"></script>
-    <script src="js/custom.js"></script> -->
   </body>
 </html>
