7d41cbcc1f5a01d20efb1a91bc66bf236b366140
littlejietu <littlejietu@qq.com>
2016-07-26 20:31:03 +0800

    service

---
+++ application/views/front/service_ios.php
@@ -0,0 +1,169 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="ico/favicon.ico">
+
+    <title>服务 - 杭州米荣网络科技有限公司</title>
+
+    <?php echo _get_html_cssjs('css','style.css,font-awesome.min.css,animate.css','css');?>
+    <?php echo _get_html_cssjs('font','PT+Sans+Narrow/pt.css','css');?>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+  <?php include("inc/nav.php");?>
+
+    <!-- Wrapper -->
+    <div class="wrapper">
+	
+	  <!-- Topic Header -->
+      <div class="topic">
+        <div class="container">
+          <div class="row">            
+            <ol class="breadcrumb hidden-xs">
+              <li><a href="<?php echo base_url();?>">首页</a></li>
+              <li class="active"><?php echo $nav['page_title']?></li>
+            </ol>                      
+          </div> <!-- / .row -->
+        </div> <!-- / .container -->
+      </div> <!-- / .Topic Header -->     
+
+      <div class="container">
+
+	    
+	    <div class="row">  
+
+          <div class="platform-advantage">
+            <div class="subject-title">
+                <h3>
+                    IOS APP
+                    <span>
+                        3
+                    </span>
+                    大优势
+                </h3>
+                <p>
+                    THE ADVANTAGES of iOS
+                </p>
+            </div>
+            <div class="platform-subject-item">
+                <div class="item-left">
+                    <img src="<?php echo _get_cfg_path('img');?>p-ios02.png" alt="手机IOS APP应用软件的营收更容易">
+                </div>
+                <div class="item-right">
+                    <h3>
+                        IOS APP营收更容易
+                        <span>
+                            EASY REVENUES
+                        </span>
+                    </h3>
+                    <div class="line">
+                    </div>
+                    <p>
+                        iPhone的用户更倾向于为中意的 App 付费，一部分原因可能由于 iPhone 用户的确比较富足，另一部分原因则在于 iTunes 与信用卡之间的整合，其支付方式十分方便。
+                        <br>
+                        相同的一个应用，同时在安卓与苹果平台上发布运营，其用户愿意进行消费的，基本整体倾向于苹果用户。
+                    </p>
+                </div>
+            </div>
+            <div class="platform-subject-item ">
+                <div class="item-left  item-left02">
+                    <img src="<?php echo _get_cfg_path('img');?>p-ios03.png" alt="IOS应用软件的发布比较统一，而且每个App应用是唯一的">
+                </div>
+                <div class="item-right">
+                    <h3>
+                        IOS应用发布统一也唯一
+                        <span>
+                            UNIFIED RELEAES
+                        </span>
+                    </h3>
+                    <div class="line">
+                    </div>
+                    <p>
+                        苹果用户想要在iphone、ipad、mac机上安装任何的应用，都只能在App Store应用商店中下载，用户在这里下载的所有记录和评论都为应用成为一个很好的背书，相对于安卓有海量的应用商店，而苹果的唯一性，很大程度的减少了用户找不少应用的问题，同时也减少了应用方在多平台上维护的费用。
+                    </p>
+                </div>
+            </div>
+            <div class="platform-subject-item">
+                <div class="item-left item-left03">
+                    <img src="<?php echo _get_cfg_path('img');?>p-ios04.png" alt="苹果IOS系统的机型比较少，而且该系统的适配性是最好的">
+                </div>
+                <div class="item-right">
+                    <h3>
+                        IOS系统机型少、适配性最优
+                        <span>
+                            HIGHLY ADAPTIVE
+                        </span>
+                    </h3>
+                    <div class="line">
+                    </div>
+                    <p>
+                        iPhone的用户更倾向于为中意的 App 付费，一部分原因可能由于 iPhone 用户的确比较富足，另一部分原因则在于 iTunes 与信用卡之间的整合，其支付方式十分方便。
+                        <br>
+                        相同的一个应用，同时在安卓与苹果平台上发布运营，其用户愿意进行消费的，基本整体倾向于苹果用户。
+                    </p>
+                </div>
+            </div>
+            <div class="platform-ios-sec-bg">
+            </div>
+        </div>
+
+        </div> 
+
+        <div class="row">
+		  <div class="col-sm-12">            
+			<div class="platform-wrap wrap">
+				<div class="app-faq">
+		            <div class="faq-wrap">
+		                <h3 class="title">
+		                    <a>
+		                        IOS开发常见入门
+		                    </a>
+		                </h3>
+		                <p class="title-sec">
+		                    FAQ
+		                </p>
+		                <ul>
+		                    <li>
+							    <h3><span></span><a href="http://www.sunnsoft.com/faq-137.html" title="谁都能做app？别把app软件开发门槛想得太低">IOS和安卓哪个更有前景？</a></h3>
+							    <p>IOS和Android的开发难度对比：Android系统大多数都是使用java语言开发的，用在Dalvik虚拟机上，其中的很少部分用到NDK，从开发上讲相对来说比较容易。IOS是用oc来开发的，要学IOS，不只是掌握了OC基础就可以了，还要会Macos/Darwin、c/c++等高级编程，从开发门槛来说，IOS比较高。</p>
+							</li>
+							<li>
+							    <h3><span></span><a href="http://www.sunnsoft.com/faq-174.html" title="如何建设一个好的网站">开发iOS软件一般用什么编程语言？</a></h3>
+							    <p>IOS软件开发是基于C语言的开发，常用的编程语言是Objective-C语言。</p>
+							</li>
+							<li>
+							    <h3><span></span><a href="http://www.sunnsoft.com/ios-8.html" title="网页设计和网站建设的区别是什么">IOS APP开发的前景是什么样的？</a></h3>
+							    <p>现在是移动互联网的时代，也是app时代。很多it人事开始投入开发app应用软件了，其中IOS开发是app开发的一部分。目前，IOS app开发是最主要的开发方向，现在很人都疯狂追捧苹果手机（iPhone），他们使用的是IPhone4，或者是IPhone5高端智能手机。这些手机有很多应用软件都是可以开发的。</p>
+							</li>
+		                </ul>
+		            </div>
+		        </div>
+
+		        
+
+		        
+		        
+		    </div>          
+          </div>
+        </div> <!-- / .row -->
+
+		
+      </div> <!-- / .container -->
+
+    </div> <!-- / .wrapper -->
+
+    <?php include("inc/footer.php");?>
+
+  </body>
+</html>
---
+++ application/views/front/service_wx.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="ico/favicon.ico">
+
+    <title>服务 - 杭州米荣网络科技有限公司</title>
+
+    <?php echo _get_html_cssjs('css','style.css,font-awesome.min.css,animate.css','css');?>
+    <?php echo _get_html_cssjs('font','PT+Sans+Narrow/pt.css','css');?>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+  <?php include("inc/nav.php");?>
+
+    <!-- Wrapper -->
+    <div class="wrapper">
+	
+	  <!-- Topic Header -->
+      <div class="topic">
+        <div class="container">
+          <div class="row">            
+            <ol class="breadcrumb hidden-xs">
+              <li><a href="<?php echo base_url();?>">首页</a></li>
+              <li class="active"><?php echo $nav['page_title']?></li>
+            </ol>                      
+          </div> <!-- / .row -->
+        </div> <!-- / .container -->
+      </div> <!-- / .Topic Header -->     
+
+      <div class="container">
+
+	    
+	    <div class="row">  
+
+          <div class="platform-advantage">
+            <div class="subject-title">
+                <h3>
+                    Android APP
+                    <span>
+                        3
+                    </span>
+                    大开发优势
+                </h3>
+                <p>
+                    THE ADVANTAGES of Android
+                </p>
+            </div>
+            <div class="platform-subject-item">
+                <div class="item-left">
+                    <img src="<?php echo _get_cfg_path('img');?>p-android02.png" alt="安卓手机系统是目前移动终端用户量最多的操作系统">
+                </div>
+                <div class="item-right">
+                    <h3>
+                        Android是用户量最多的移动操作系统
+                        <span>
+                            THE MOST USERS
+                        </span>
+                    </h3>
+                    <div class="line">
+                    </div>
+                    <p>
+                        就在2013年5月，安卓系统诞生5周年的时候，全球采用安卓系统的移动设备，已经突破了10亿台，可以说是您身边6台就会至少有一台设备用着这个系统。
+                    </p>
+                </div>
+            </div>
+            <div class="platform-subject-item">
+                <div class="item-left">
+                    <img src="<?php echo _get_cfg_path('img');?>p-android03.png" alt="安卓手机app应用软件推广起来相对容易">
+                </div>
+                <div class="item-right">
+                    <h3>
+                        Android APP 推广相对容易
+                        <span>
+                            EASY SEO
+                        </span>
+                    </h3>
+                    <div class="line">
+                    </div>
+                    <p>
+                        相对苹果用户想要下载应用只能通过app store，安卓用户的下载就相对容易非常多，用户不仅可以通过应用商店下载您的应用，甚至可以在您推广的广告、宣传单张中扫一个二维码就可以开始下载您的应用。
+                    </p>
+                </div>
+            </div>
+            <div class="platform-subject-item">
+                <div class="item-left">
+                    <img src="<?php echo _get_cfg_path('img');?>p-android04.png" alt="Android系统的开放性比较高">
+                </div>
+                <div class="item-right">
+                    <h3>
+                        Android 系统开放性高
+                        <span>
+                            HIGHLY OPENING SYSTEM
+                        </span>
+                    </h3>
+                    <div class="line">
+                    </div>
+                    <p>
+                        相对苹果系统的全封闭式，安卓系统的高度开放，给了开发者相当高的开发自由度，您的应用可以更加接近您所想所要的。
+                    </p>
+                </div>
+            </div>
+            <div class="platform-ios-sec-bg">
+            </div>
+          </div>
+
+        </div> 
+
+        <div class="row">
+		  <div class="col-sm-12">            
+			<div class="platform-wrap wrap">
+				<div class="app-faq">
+		            <div class="faq-wrap">
+		                <h3 class="title">
+		                    <a>
+		                        Android开发常见入门
+		                    </a>
+		                </h3>
+		                <p class="title-sec">
+		                    FAQ
+		                </p>
+		                <ul>
+		                    <li>
+							    <h3><span></span><a href="http://www.sunnsoft.com/faq-47.html" title="谁都能做app？别把app软件开发门槛想得太低">Android有哪些优势？</a></h3>
+							    <p>开放性——开放的平台允许任何移动终端厂商加入安卓联盟中来，拥有更多的开发者，让Android这个平台也将很快走向成熟。<br />丰富的硬件——Android的开放性使得众多厂商推出各具功能特色的产品。<br />方便开发——Android为第三方开发商提供自由宽泛的环境，让更多的新颖别致的软件得以诞生。</p>
+							</li>
+							<li>
+							    <h3><span></span><a href="http://www.sunnsoft.com/faq-112.html" title="如何建设一个好的网站">Android软件开发用什么语言？</a></h3>
+							    <p>主流的开发语言是Java语言，使接口到功能，都有层出不穷的变化。提高了软件的交互的可能性是Java最大的特性，可以说安卓手机几乎所有的应用程序都是利用Java语言来进行编写的。使用Java语言开发的软件的程序库、数据库、运行库都是Android手机软件的一大特点。</p>
+							</li>
+							<li>
+							    <h3><span></span><a href="http://www.sunnsoft.com/faq-96.html" title="网页设计和网站建设的区别是什么">Android安卓应用推广方法有哪些？</a></h3>
+							    <p>1、提交到各大安卓应用市场，2、付费预装APP应用推广，3、打造优质APP利用口碑推广，4、与各大手机论坛合作，5、媒体的报道和品牌曝光，6、与其他应用互相推广</p>
+							</li>
+		                </ul>
+		            </div>
+		        </div>
+
+		        
+
+		        
+		        
+		    </div>          
+          </div>
+        </div> <!-- / .row -->
+
+		
+      </div> <!-- / .container -->
+
+    </div> <!-- / .wrapper -->
+
+    <?php include("inc/footer.php");?>
+
+  </body>
+</html>
