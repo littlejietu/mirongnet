2b06b3e9721841ea02df6c972131362558285f80
littlejietu <littlejietu@qq.com>
2016-07-26 20:37:24 +0800

    service

---
+++ application/views/front/inc/footer.php
@@ -0,0 +1,69 @@
+<footer>
+      <div class="container">
+        <div class="row">
+		
+          <!-- 联系我们 -->
+          <div class="col-xs-12 col-sm-3 col-md-3">
+            <h4>联系我们</h4>            
+			<p>
+			  杭州经济开发区 <br />
+			  <br /><br />
+			  电话 : 159 6885 0531<br />
+			  Email : xyy@mirongnet.com
+
+			</p>			
+          </div><!-- / .联系我们 -->
+		  
+		  <!-- 有用的链接 -->
+          <div class="col-xs-12 col-sm-3 col-md-3">            
+            <h4>APP服务</h4>
+            <p>
+              <a href="#">IOS开发</a><br />
+              <a href="#">Android开发</a><br />
+              <a href="#">微信公众号开发</a><br />
+              <a href="#">H5手机站开发</a><br />
+              <a href="#">Web网站开发</a><br />
+              <a href="#">API开发</a><br />
+            </p>
+          </div><!-- / .有用的链接 -->
+		 
+          <!-- Recent Tweets -->
+          <div class="col-xs-12 col-sm-3 col-md-3">
+            <h4>案例集</h4>
+			<div class="gallery clearfix">
+			 <ul class="list-unstyled">
+			  <li><a href="#"><img src="<?php echo _get_cfg_path('img');?>avatar-1.jpg" style="width:60px;height:60px;" alt="..."></a></li>
+				<li><a href="#"><img src="<?php echo _get_cfg_path('img');?>avatar-2.jpg" style="width:60px;height:60px;" alt="..."></a></li>
+				<li><a href="#"><img src="<?php echo _get_cfg_path('img');?>avatar-3.jpg" style="width:60px;height:60px;" alt="..."></a></li>
+				<li><a href="#"><img src="<?php echo _get_cfg_path('img');?>avatar-4.jpg" style="width:60px;height:60px;" alt="..."></a></li>
+				<li><a href="#"><img src="<?php echo _get_cfg_path('img');?>avatar-5.jpg" style="width:60px;height:60px;" alt="..."></a></li>
+				<li><a href="#"><img src="<?php echo _get_cfg_path('img');?>avatar-6.jpg" style="width:60px;height:60px;" alt="..."></a></li>
+								
+			 </ul>
+			</div>
+          </div><!-- / .Recent Tweets -->
+		  
+          <!-- 通讯 -->
+          <div class="col-xs-12 col-sm-3 col-md-3">
+            <h4>米荣</h4>
+            <p>
+              打造国内最专业的手机APP软件开发公司              
+            </p>
+            
+			<br />
+			<h4>Follow us on</h4>
+			  <ul class="list-inline">
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+				<li><a href="#"><i class="fa fa-envelope"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="#"><i class="fa fa-skype"></i></a></li>				
+			  </ul>
+          </div><!-- / .通讯 -->
+		  
+        </div> <!-- / .row -->		
+      </div> <!-- / .container --> 	  
+    </footer><!-- / .Footer -->
+  
